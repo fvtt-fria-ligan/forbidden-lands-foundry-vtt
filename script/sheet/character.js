e1f0e19da536c6cd3ca9a9cdd32005e5673307c0
Perfectro <launay_g@epitech.eu>
2020-06-09 18:54:14 +0200

    Add listener on character sheet

--- script/sheet/character.js
+++ script/sheet/character.js
@@ -35,10 +35,62 @@ export class ForbiddenLandsCharacterSheet extends ActorSheet {
         });
         html.find('.condition').click(ev => {
             const conditionName = $(ev.currentTarget).data("key");
-            const conditionValue = this.actor.data.data.condition[conditionName].value
+            const conditionValue = this.actor.data.data.condition[conditionName].value;
             this.actor.data.data.condition[conditionName].value = !conditionValue;
             this._render();
         });
+        html.find('.attribute b').click(ev => {
+            const div = $(ev.currentTarget).parents(".attribute");
+            const attributeName = div.data("key");
+            const attribute = this.actor.data.data.attribute[attributeName];
+            console.log(attribute);
+        });
+        html.find('.skill b').click(ev => {
+            const div = $(ev.currentTarget).parents(".skill");
+            const skillName = div.data("key");
+            const skill = this.actor.data.data.skill[skillName];
+            const attribute = this.actor.data.data.attribute[skill.attribute];
+            console.log(skill);
+            console.log(attribute);
+        });
+        html.find('.armor b').click(ev => {
+            const div = $(ev.currentTarget).parents(".armor");
+            const armorName = div.data("key");
+            const armor = this.actor.data.data.armor[armorName];
+            console.log(armor);
+        });
+        html.find('.weapon.item .name').click(ev => {
+            const div = $(ev.currentTarget).parents(".item");
+            const weapon = this.actor.getOwnedItem(div.data("itemId"));
+            console.log(weapon);
+        });
+        html.find('.critical-injury.item .name').click(ev => {
+            const div = $(ev.currentTarget).parents(".item");
+            const criticalInjury = this.actor.getOwnedItem(div.data("itemId"));
+            console.log(criticalInjury);
+        });
+        html.find('.talent.item .name').click(ev => {
+            const div = $(ev.currentTarget).parents(".item");
+            const talent = this.actor.getOwnedItem(div.data("itemId"));
+            console.log(talent);
+        });
+        html.find('.spell.item .name').click(ev => {
+            const div = $(ev.currentTarget).parents(".item");
+            const spell = this.actor.getOwnedItem(div.data("itemId"));
+            console.log(spell);
+        });
+        html.find('.gear.item .name').click(ev => {
+            const div = $(ev.currentTarget).parents(".item");
+            const gear = this.actor.getOwnedItem(div.data("itemId"));
+            console.log(gear);
+        });
+        html.find('.consumable b').click(ev => {
+            const div = $(ev.currentTarget).parents(".consumable");
+            const consumableName = div.data("key");
+            const consumable = this.actor.data.data.consumable[consumableName];
+            console.log(consumable);
+        });
+
     }
 
     computeSkills(data) {
