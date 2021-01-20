c8660a2f3797ce671be22ef9f7ecb1f28e9b7642
Mark Fodor <akosmark.fodor@gmail.com>
2021-01-20 16:58:48 +0100

    Added the option to change the maximum encumbrance of characters on the sheet

--- script/sheet/character.js
+++ script/sheet/character.js
@@ -147,10 +147,10 @@ export class ForbiddenLandsCharacterSheet extends ForbiddenLandsActorSheet {
     }
     const coinsCarried = parseInt(data.data.currency.gold.value) + parseInt(data.data.currency.silver.value) + parseInt(data.data.currency.copper.value);
     weightCarried += Math.floor(coinsCarried / 100) * 0.5;
-    const weightAllowed = data.data.attribute.strength.max * 2;
+    const weightAllowed = data.data.attribute.strength.max * 2 + (data.data.extraEncumbrance ?? 0);
     data.data.encumbrance = {
       value: weightCarried,
-      max: weightAllowed,
+      max: weightAllowed - (data.data.extraEncumbrance ?? 0),
       over: weightCarried > weightAllowed,
     };
   }
