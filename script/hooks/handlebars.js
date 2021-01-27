91ff573aa5dbf76d907a70649821abd560fc2365
Pierre Sudron <pierre@giant-teapot.org>
2021-01-27 08:54:45 +0100

    Add add features toggles in the weapon sheet
    Change the weapon features from a single string to an object containing
    all features available in the core rules.
     * melee weapon features: edged, pointed, blunt, parrying and hook
     * ranged weapon feature: slow reload
    
    A free-form list is left to allow custom features to be added to
    weapons.
    
    The weapon sheet was modified to let users select the weapon features
    via toggles. Available toggles depend on the current weapon's category
    (melee or ranged).
    
    There are also some changes in the way weapons are posted to the chat,
    mostly to allow the localization of stock weapon features.

--- script/hooks/handlebars.js
+++ script/hooks/handlebars.js
@@ -145,6 +145,61 @@ function registerHandlebarsHelpers() {
     });
     return output.join(", ");
   });
+
+  Handlebars.registerHelper("hasWeaponFeatures", function (weaponType, features) {
+    const meleeFeatures = ["edged", "pointed", "blunt", "parry", "hook"];
+    const rangedFeatures = ["slowReload"];
+
+    if (features.others !=="") {
+      return true;
+    } 
+
+    let weaponFeatures = [];
+    if(weaponType == "melee") {
+      weaponFeatures = meleeFeatures;
+    }
+    else if (weaponType == "ranged") {
+      weaponFeatures = rangedFeatures;
+    }
+
+    for (const feature in features) {
+      if (weaponFeatures.includes(feature) && features[feature]) {
+        return true;
+      }
+    }
+    return false;
+  });
+
+  Handlebars.registerHelper("formatWeaponFeatures", function (weaponType, features) {
+    let output = [];
+    if(weaponType === "melee") {
+      if (features.edged) {
+        output.push(game.i18n.localize("WEAPON.FEATURES.EDGED"));
+      }
+      if (features.pointed) {
+        output.push(game.i18n.localize("WEAPON.FEATURES.POINTED"));
+      }
+      if (features.blunt) {
+        output.push(game.i18n.localize("WEAPON.FEATURES.BLUNT"));
+      }
+      if (features.parrying) {
+        output.push(game.i18n.localize("WEAPON.FEATURES.PARRYING"));
+      }
+      if (features.hook) {
+        output.push(game.i18n.localize("WEAPON.FEATURES.HOOK"));
+      }
+    }
+    if (weaponType === "ranged") {
+      if (features.slowReload) {
+        output.push(game.i18n.localize("WEAPON.FEATURES.SLOW_RELOAD"));
+      }
+    }
+    if (features.others !== "") {
+      output.push(features.others)
+    }
+    return output.join(", ");
+  });
+  
   Handlebars.registerHelper("plaintextToHTML", function (value) {
     // strip tags, add <br/> tags
     return new Handlebars.SafeString(value.replace(/(<([^>]+)>)/gi, "").replace(/(?:\r\n|\r|\n)/g, "<br/>"));
