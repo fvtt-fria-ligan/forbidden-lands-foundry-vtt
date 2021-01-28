19f72e762d3cc27164b104b093c5d9fe14b1299d
Pierre Sudron <pierre@giant-teapot.org>
2021-01-28 13:04:48 +0100

    Redesign the weapon sheet's header
    This commit has a few visual improvements on the "header" part of the
    weapon sheet. It is based on the design of the current character sheet.
    
    New weapon sheet features:
     * clickable marks to increase/decrease the weapon bonus (wear/repair)
     * the artifact die is available as a dropdown menu
    
    This commit adds new strings for the artefact die selector. Translations
    are available for:
     * en
     * fr
     * sv (courtesy of Petter NystrÃm)

--- script/sheet/weapon.js
+++ script/sheet/weapon.js
@@ -6,4 +6,23 @@ export class ForbiddenLandsWeaponSheet extends ForbiddenLandsItemSheet {
       template: "systems/forbidden-lands/model/weapon.html",
     });
   }
+
+  activateListeners(html) {
+    super.activateListeners(html);
+
+    // Weapon bonus markers
+    html.find(".change-bonus").on("click contextmenu", (ev) => {
+      const bonus = this.object.data.data.bonus;
+      let value = bonus.value;
+      if (ev.type === "click") {
+        value = Math.max(value - 1, 0);
+      } else if (ev.type === "contextmenu") {
+        value = Math.min(value + 1, bonus.max);
+      }
+      this.object.update({
+        ["data.bonus.value"]: value,
+      });
+    });
+  }
+
 }
