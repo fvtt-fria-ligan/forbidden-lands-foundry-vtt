6b505f1a9969a5c0b5e6ec7fb5816a8dbd7fadaf
aMediocreDad <amediocredad@outlook.com>
2021-02-10 21:47:41 +0100

    Added tabs, Artifact dropdown and rearranged boxes

--- script/sheet/armor.js
+++ script/sheet/armor.js
@@ -4,6 +4,28 @@ export class ForbiddenLandsArmorSheet extends ForbiddenLandsItemSheet {
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
       template: "systems/forbidden-lands/model/armor.html",
+      tabs: [
+        {
+          navSelector: ".sheet-tabs",
+          contentSelector: ".sheet-body",
+          initial: "attributes",
+        },
+      ],
     });
+  }
+
+  computeQuality(data) {
+    data.artifact = data.data.artifactBonus !== "";
+  }
+
+  getData() {
+    const data = super.getData();
+    this.computeQuality(data);
+    return data;
+  }
+
+  async _renderInner(data, options) {
+    data.showAppearanceField = game.settings.get("forbidden-lands", "showAppearanceField");
+    return super._renderInner(data, options);
   }
 }
--- script/sheet/gear.js
+++ script/sheet/gear.js
@@ -4,6 +4,28 @@ export class ForbiddenLandsGearSheet extends ForbiddenLandsItemSheet {
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
       template: "systems/forbidden-lands/model/gear.html",
+      tabs: [
+        {
+          navSelector: ".sheet-tabs",
+          contentSelector: ".sheet-body",
+          initial: "attributes",
+        },
+      ],
     });
+  }
+
+  computeQuality(data) {
+    data.artifact = data.data.artifactBonus !== "";
+  }
+
+  getData() {
+    const data = super.getData();
+    this.computeQuality(data);
+    return data;
+  }
+
+  async _renderInner(data, options) {
+    data.showAppearanceField = game.settings.get("forbidden-lands", "showAppearanceField");
+    return super._renderInner(data, options);
   }
 }
