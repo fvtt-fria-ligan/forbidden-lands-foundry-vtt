78dcc655cb31d3b9eaa7ffd0ee56ce1e7964fb77
aMediocreDad <amediocredad@outlook.com>
2021-02-14 00:30:17 +0100

    Modules: - Modules edited to support functionality of item changes. - Consolidated logic to parent entity item.js
    template.json
    - Added "artifact" object to templates and moved relevant properties to this object.
    - Added type property to talents.

--- script/sheet/armor.js
+++ script/sheet/armor.js
@@ -4,6 +4,13 @@ export class ForbiddenLandsArmorSheet extends ForbiddenLandsItemSheet {
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
       template: "systems/forbidden-lands/model/armor.html",
+      tabs: [
+        {
+          navSelector: ".sheet-tabs",
+          contentSelector: ".sheet-body",
+          initial: "main",
+        },
+      ],
     });
   }
 }
