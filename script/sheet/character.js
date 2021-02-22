1477fedc0fd120419e91478c3151de1d107ab6ab
aMediocreDad <amediocredad@outlook.com>
2021-02-22 14:40:15 +0100

    Added encumbrance for items up to 8

--- script/sheet/character.js
+++ script/sheet/character.js
@@ -155,6 +155,18 @@ export class ForbiddenLandsCharacterSheet extends ForbiddenLandsActorSheet {
             return 0.5;
           case "heavy":
             return 2;
+          case "3":
+            return 3;
+          case "4":
+            return 4;
+          case "5":
+            return 5;
+          case "6":
+            return 6;
+          case "7":
+            return 7;
+          case "8":
+            return 8;
           default:
             return 1;
         }
