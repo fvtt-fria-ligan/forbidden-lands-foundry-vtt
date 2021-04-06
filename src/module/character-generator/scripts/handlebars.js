4a3451e43c6d0e207eef2069e94372f671fee8c9
Maxter <maxim.starovoitov@gmail.com>
2021-04-06 21:28:53 +0300

    character generator as is

---
+++ src/module/character-generator/scripts/handlebars.js
@@ -0,0 +1,29 @@
+
+function preloadHandlebarsTemplates() {
+    const templatePaths = [
+        "modules/forbidden-lands-character-generator/templates/partial/list.html",
+    ];
+    return loadTemplates(templatePaths);
+}
+
+function registerHandlebarsHelpers() {
+    Handlebars.registerHelper('flps_capitalize', function (value, options) {
+        return typeof value === 'string' && value.length > 0 ? value[0].toUpperCase() + value.slice(1) : value;
+    });
+    Handlebars.registerHelper('flps_strconcat', function () {
+        const args = Array.prototype.slice.call(arguments);
+        args.pop(); // remove unrelated data
+        return args.join("");
+    });
+
+    Handlebars.registerHelper('flps_enrich', function (content) {
+        // Enrich the content
+        content = TextEditor.enrichHTML(content, { entities: true });
+        return new Handlebars.SafeString(content);
+    });
+}
+
+export const initializeHandlebars = () => {
+  registerHandlebarsHelpers();
+  preloadHandlebarsTemplates();
+};
