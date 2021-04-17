84a0a76a4b19870ad5054f51029d44490b308592
aMediocreDad <amediocredad@outlook.com>
2021-04-17 19:05:35 +0200

    build: 📦️ Extensive changes to the build process
    Mostly changes to the gulpfile which now can push a release tag to remote.
    A github action takes care of adding artifacts to the release when published.

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,18 @@
 const { nodeResolve } = require("@rollup/plugin-node-resolve");
+const dotenv = require("dotenv");
+const result = dotenv.config();
+const env = process.env.NODE_ENV || "development";
+const { terser } = require("rollup-plugin-terser");
 
+if (result.error) {
+	throw result.error;
+}
 module.exports = {
 	input: "src/module/forbidden-lands.js",
 	output: {
 		dir: "dist/module",
 		format: "es",
-		sourcemap: true,
+		sourcemap: env === "development" ? true : false,
 	},
-	plugins: [nodeResolve()],
+	plugins: [nodeResolve(), env === "production" ? terser() : null],
 };
