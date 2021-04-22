a292445f483e4a1b7c229c52c9c3eb50d695499c
aMediocreDad <amediocredad@outlook.com>
2021-04-22 15:57:32 +0200

    feat: ✨ Changed how datasets are loaded
    In this commit I reworked the setting for getting custom datasets
    The setting now loads a dataset as opposed to pointing to one.
    
    Signed-off-by: aMediocreDad <amediocredad@outlook.com>

--- src/module/system/settings.js
+++ src/module/system/settings.js
@@ -1,3 +1,52 @@
+class importDatasetDialog extends FormApplication {
+	render() {
+		new Dialog(
+			{
+				title: `Import Custom Dataset`,
+				content: `<form autocomplete="off" onsubmit="event.preventDefault();">
+					<p class="notes">You may import a dataset from a JSON file.</p>
+					<p class="notes">This operation will configure the dataset used in the character generator.</p>
+					<div class="form-group">
+						<label for="data">Source Data</label>
+						<input type="file" name="data" />
+					</div>
+				</form>`,
+				buttons: {
+					import: {
+						icon: '<i class="fas fa-file-import"></i>',
+						label: "Import",
+						callback: async (html) => {
+							const form = html.find("form")[0];
+							if (!form.data.files.length)
+								return ui.notifications.error("You did not upload a data file!");
+							game.fbl.config.dataset = await readTextFromFile(form.data.files[0]).then((json) =>
+								JSON.parse(json),
+							);
+							return true;
+						},
+					},
+					reset: {
+						icon: '<i class="fas fa-undo-alt"></i>',
+						label: game.i18n.localize("DIALOG.RESET"),
+						callback: () => {
+							game.fbl.config.dataset = null;
+							return false;
+						},
+					},
+					no: {
+						icon: '<i class="fas fa-times"></i>',
+						label: "Cancel",
+					},
+				},
+				default: "import",
+			},
+			{
+				width: 400,
+			},
+		).render(true);
+	}
+}
+
 export default function registerSettings() {
 	game.settings.register("forbidden-lands", "worldSchemaVersion", {
 		name: "World Version",
@@ -23,13 +72,12 @@ export default function registerSettings() {
 		default: false,
 		type: Boolean,
 	});
-	game.settings.register("forbidden-lands", "datasetDir", {
+	game.settings.registerMenu("forbidden-lands", "datasetDir", {
 		name: game.i18n.localize("FLCG.SETTINGS.DATASET_DIR"),
 		hint: game.i18n.localize("FLCG.SETTINGS.DATASET_DIR_HINT"),
-		scope: "world",
-		config: true,
-		default: "systems/forbidden-lands/assets",
-		type: window.Azzu.SettingsTypes.DirectoryPicker,
+		icon: "fas fa-file-import",
+		restricted: true,
+		type: importDatasetDialog,
 	});
 	game.settings.register("forbidden-lands", "showCraftingFields", {
 		name: "CONFIG.CRAFTINGFIELD",
