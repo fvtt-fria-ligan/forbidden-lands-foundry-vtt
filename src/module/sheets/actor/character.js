139722580bd03f45108bbe9a6123c6546d3a8f03
aMediocreDad <amediocredad@outlook.com>
2021-09-11 18:40:47 +0200

    fix: 🐛 Fix Encumbrance not calculating without a modifier
    Signed-off-by: aMediocreDad <amediocredad@outlook.com>

--- src/module/sheets/actor/character.js
+++ src/module/sheets/actor/character.js
@@ -117,9 +117,7 @@ export class ForbiddenLandsCharacterSheet extends ForbiddenLandsActorSheet {
 			parseInt(data.data.currency.copper.value);
 		weightCarried += Math.floor(coinsCarried / 100) * 0.5;
 		const modifiers = this.actor.getRollModifierOptions("carryingCapacity");
-		const weightAllowed = modifiers
-			? data.data.attribute.strength.max * 2 + parseInt(modifiers[0]?.value) || 0
-			: data.data.attribute.strength.max * 2;
+		const weightAllowed = data.data.attribute.strength.max * 2 + (parseInt(modifiers[0]?.value) || 0);
 		data.data.encumbrance = {
 			value: weightCarried,
 			max: weightAllowed,
