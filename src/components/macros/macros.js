b2ca3cd516d6bc47d776c7df1109933b897a9c30
aMediocreDad <amediocredad@outlook.com>
2022-07-15 22:01:52 +0200

    feat: ✨ Add hotbardrop macros

---
+++ src/components/macros/macros.js
@@ -0,0 +1,110 @@
+import localizeString from "@utils/localize-string.js";
+
+const handlers = {
+	Actor: handleActorMacro,
+	Item: handleItemMacro,
+};
+
+const itemHandlers = {
+	spell: handleSpellMacro,
+	weapon: handleWeaponMacro,
+	armor: handleWeaponMacro,
+};
+
+function handleSpellMacro(actor, item) {
+	return {
+		command: `game.actors.get("${actor.id}").sheet.rollSpell("${item._id}")`,
+		img: item.img,
+		name: item.name,
+		type: "script",
+	};
+}
+
+async function handleWeaponMacro(actor, item) {
+	const type =
+		item.data.part === "shield" || item.data.category === "melee"
+			? await Dialog.prompt({
+					title: game.i18n.localize("MACRO.CHOOSE_TYPE"),
+					content: `
+						<form class="macro-select">
+						<h3>${game.i18n.localize("MACRO.CHOOSE_TYPE")}</h3>
+							<label>
+							<input type="radio" name="type" value="gear" checked />
+							${game.i18n.localize("MACRO.TYPE.ATTACK")}</label>
+							<label>
+							<input type="radio" name="type" value="parry" />
+							${game.i18n.localize("ACTION.PARRY")}</label>
+							${
+								item.data.part === "shield"
+									? `<label>
+							<input type="radio" name="type" value="shove" />
+							${game.i18n.localize("ACTION.SHOVE")}</label>`
+									: ""
+							}
+							${
+								item.data.category === "melee"
+									? `<label>
+							<input type="radio" name="type" value="disarm" />
+							${game.i18n.localize("ACTION.DISARM")}</label>`
+									: ""
+							}
+						</form>
+					`,
+					callback: (html) => {
+						const form = html.find("form")[0];
+						const data = new FormData(form);
+						return data.get("type");
+					},
+					rejectClose: true,
+			  })
+			: "gear";
+	const command =
+		type === "gear"
+			? `game.actors.get("${actor.id}").sheet.rollGear("${item._id}")`
+			: `game.actors.get("${actor.id}").sheet.rollAction("${type}","${item._id}")`;
+	// eslint-disable-next-line no-shadow
+	const name = type === "gear" ? item.name : `${item.name}: ${localizeString(type)}`;
+	return {
+		command,
+		name,
+		img: item.img,
+		type: "script",
+	};
+}
+
+async function handleActorMacro(data) {
+	const actor = game.actors.get(data.id);
+	const imgs = await actor.getTokenImages();
+	return {
+		command: `game.actors.get("${data.id}").sheet.render(true)`,
+		img: imgs[0],
+		name: actor.name,
+		type: "script",
+	};
+}
+
+async function handleItemMacro(data) {
+	const handler = itemHandlers[data.data.type];
+	if (!handler) return {};
+	const actor = game.actors.get(data.actorId);
+	const item = data.data;
+	return handler(actor, item);
+}
+
+export default async function handleHotbarDrop(data, slot) {
+	const handler = handlers[data.type];
+	if (!handler) return;
+	// eslint-disable-next-line no-shadow
+	const { command, img, name, type } = await handler(data);
+	if (!name || !command) return;
+	let macro = game.macros.contents.find((m) => m.name === name && m.command === command);
+	if (!macro) {
+		macro = await Macro.create({
+			command,
+			img,
+			name,
+			type,
+		});
+	}
+	game.user.assignHotbarMacro(macro, slot);
+}
