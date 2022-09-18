0e9281787b34425efff350eac5191ff77c9d872c
aMediocreDad <amediocredad@outlook.com>
2022-09-18 20:56:28 +0200

    fix: 🐛 Fix editor issues in item/actor sheets
    closes: #261

--- src/system/core/foundry-overrides.js
+++ src/system/core/foundry-overrides.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars, no-shadow */
-export default function FoundryOverrides() {
-	FormApplication.prototype.activateEditor = function (name, options = {}, initialContent = "") {
+export default async function FoundryOverrides() {
+	FormApplication.prototype.activateEditor = async function (name, options = {}, initialContent = "") {
 		const editor = this.editors[name];
 		if (!editor) throw new Error(`${name} is not a registered editor name!`);
 		options = foundry.utils.mergeObject(editor.options, options);
@@ -10,12 +10,20 @@ export default function FoundryOverrides() {
 			options.plugins = "lists quickbars hr code save";
 			options.save_enablewhendirty = false;
 		}
-		TextEditor.create(options, initialContent || editor.initial).then((mce) => {
-			editor.mce = mce;
-			editor.changed = false;
-			editor.active = true;
-			mce.focus();
-			mce.on("change", (ev) => (editor.changed = true));
-		});
+		if (!options.fitToSize) options.height = options.target.offsetHeight;
+		if (editor.hasButton) editor.button.style.display = "none";
+		const instance =
+			(editor.instance =
+			editor.mce =
+				await TextEditor.create(options, initialContent || editor.initial));
+		options.target.closest(".editor")?.classList.add(options.engine ?? "tinymce");
+		editor.changed = false;
+		editor.active = true;
+		/** @deprecated since v10 */
+		if (options.engine !== "prosemirror") {
+			instance.focus();
+			instance.on("change", () => (editor.changed = true));
+		}
+		return instance;
 	};
 }
