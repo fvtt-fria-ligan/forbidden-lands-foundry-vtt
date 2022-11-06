96ba8692f65ecc17dd81cd96378d13f2fd16d4b7
aMediocreDad <amediocredad@outlook.com>
2022-11-06 16:25:00 +0100

    fix: 🐛 Reimplement adventure sites

--- src/journal/adventure-sites/adventure-site-sheet.js
+++ src/journal/adventure-sites/adventure-site-sheet.js
@@ -1,11 +1,4 @@
 export class AdventureSitesSheet extends JournalSheet {
-	static get defaultOptions() {
-		return mergeObject(super.defaultOptions, {
-			classes: ["journal-sheet", "adventure-sites"],
-			scrollY: [".editor-content"],
-		});
-	}
-
 	get template() {
 		return "systems/forbidden-lands/templates/journal/adventure-sites/adventure-site-sheet.hbs";
 	}
@@ -18,31 +11,14 @@ export class AdventureSitesSheet extends JournalSheet {
 
 	activateListeners(html) {
 		super.activateListeners(html);
-		html.find(".add-room").on("click", async () => {
+		html.find('[data-action="add-room"]').on("click", async () => {
 			const type = this.object.type;
 			const path = CONFIG.fbl.adventureSites.types[type];
 			const room = await CONFIG.fbl.adventureSites?.generate(path, type + "_rooms");
-			await this.object.update({ content: this.object.data.content.concat(room) });
-			console.log(this);
-			const editor = this.element.find(".editor-content");
-			editor.animate({ scrollTop: editor.prop("scrollHeight") }, 500);
-		});
-
-		const rooms = html.find(".room");
-
-		rooms.on("mouseover", (event) => {
-			const el = event.currentTarget;
-			if (el.classList.contains("selected")) return;
-			const index = Array.from(rooms).indexOf(el);
-			const indexTip = document.createElement("div");
-			indexTip.classList.add("index-tip");
-			indexTip.innerHTML = `${index + 1}`;
-			el.appendChild(indexTip);
-		});
-		rooms.on("mouseout", (event) => {
-			const el = event.currentTarget;
-			if (el.classList.contains("selected")) return;
-			$(el).find(".index-tip").remove();
+			const pageName = $(room).find("strong").first().text().replace(/\W+/, " ").trim();
+			await this.object.createEmbeddedDocuments("JournalEntryPage", [
+				{ name: pageName, title: { level: 2, show: false }, text: { content: room } },
+			]);
 		});
 	}
 }
--- src/journal/journal-document.js
+++ src/journal/journal-document.js
@@ -1,15 +1,18 @@
 export class ForbiddenLandsJournalEntry extends JournalEntry {
-	// static async create(data, options) {
-	// 	if (!data.type || data.type === "base") return super.create(data, options);
-	// 	data.flags = {
-	// 		"forbidden-lands": { type: data.type },
-	// 	};
-	// 	const path = CONFIG.fbl.adventureSites?.types[data.type];
-	// 	data.content = await CONFIG.fbl.adventureSites?.generate(path, data.type);
-	// 	super.create(data, options);
-	// }
-	// get type() {
-	// 	if (!super._getSheetClass()) return "base";
-	// 	return this.getFlag("forbidden-lands", "type") || "base";
-	// }
+	static async create(data, options) {
+		if (!data.type || data.type === "base") return super.create(data, options);
+		data.flags = {
+			"forbidden-lands": { type: data.type },
+		};
+		const path = CONFIG.fbl.adventureSites?.types[data.type];
+		const content = await CONFIG.fbl.adventureSites?.generate(path, data.type);
+		data.pages = [{ name: "Overview", title: { show: false }, text: { content } }];
+		console.log(options);
+		super.create(data, options);
+	}
+	get type() {
+		const type = this.getFlag("forbidden-lands", "type");
+		if (type) return type;
+		else return CONST.BASE_DOCUMENT_TYPE;
+	}
 }
