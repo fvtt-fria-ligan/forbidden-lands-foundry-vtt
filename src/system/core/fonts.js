9836bf1f4192106d8699f11ae1bbb060330e6486
aMediocreDad <amediocredad@outlook.com>
2023-05-25 18:47:43 +0200

    fix: 🐛 Rename all fonts

--- src/system/core/fonts.js
+++ src/system/core/fonts.js
@@ -2,17 +2,17 @@ export function registerFonts() {
 	CONFIG.fontDefinitions.Author = {
 		editor: true,
 		fonts: [
-			{ urls: ["/system/forbidden-lands/fonts/Author-SemiBold.otf"], weight: 600 },
-			{ urls: ["/system/forbidden-lands/fonts/Author-SemiBoldItalic.otf"], weight: 600, style: "italic" },
-			{ urls: ["/system/forbidden-lands/fonts/Author-Medium.otf"] },
-			{ urls: ["/system/forbidden-lands/fonts/Author-MediumItalic.otf"], style: "italic" },
+			{ urls: ["/systems/forbidden-lands/fonts/author-semibold.otf"], weight: 600 },
+			{ urls: ["/systems/forbidden-lands/fonts/author-semibold-italic.otf"], weight: 600, style: "italic" },
+			{ urls: ["/systems/forbidden-lands/fonts/author-medium.otf"] },
+			{ urls: ["/systems/forbidden-lands/fonts/author-medium-italic.otf"], style: "italic" },
 		],
 	};
 	CONFIG.fontDefinitions["IM Fell Great Primer"] = {
 		editor: true,
 		fonts: [
-			{ urls: ["/system/forbidden-lands/fonts/IMFeGPrm29C.otf"] },
-			{ urls: ["/system/forbidden-lands/fonts/IMFeGPit29C.otf"], style: "italic" },
+			{ urls: ["/systems/forbidden-lands/fonts/imfe-gprm.otf"] },
+			{ urls: ["/systems/forbidden-lands/fonts/imfe-gpit.otf"], style: "italic" },
 		],
 	};
 }
