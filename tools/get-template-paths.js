419726bcd511bd5aa23baf65d3f968fffacd403e
aMediocreDad <amediocredad@outlook.com>
2023-08-27 22:06:06 +0200

    refactor: ♻️ Move templates into root

--- tools/get-template-paths.js
+++ tools/get-template-paths.js
@@ -1,10 +1,10 @@
-import { sep, posix } from "node:path";
 import { glob } from "fs-extra-plus";
+import { posix, sep } from "node:path";
 
 export default (async () => {
-	const paths = await glob("**/*.hbs", { cwd: "src" });
+	const paths = await glob("**/*.hbs", { cwd: "." });
 	return paths.map((templatePath) => {
 		templatePath = templatePath.split(sep).slice(1).join(posix.sep);
-		return `systems/forbidden-lands/templates/${templatePath.replace("templates/", "")}`;
+		return `systems/forbidden-lands/templates/${templatePath}`;
 	});
 })();
