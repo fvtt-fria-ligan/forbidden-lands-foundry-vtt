9a60e2de5278cc02bdd0d6e26b70ac594e842f9d
aMediocreDad <amediocredad@outlook.com>
2023-08-27 22:22:19 +0200

    refactor: ♻️ Output js css at root

--- esbuild.config.js
+++ esbuild.config.js
@@ -4,40 +4,42 @@ import templatePathsPromise from "./tools/get-template-paths.js";
 
 const templatePaths = await templatePathsPromise;
 
-export default async ({ watch = false, production = false } = {}) => {
-	const ctx = await context({
-		bundle: true,
-		entryPoints: ["./src/forbidden-lands.js", "./src/forbidden-lands.scss"],
-		outdir: "dist",
-		format: "esm",
-		logLevel: "info",
-		sourcemap: !production ? "inline" : false,
-		ignoreAnnotations: !production,
-		minifyWhitespace: true,
-		minifySyntax: true,
-		drop: production ? ["console", "debugger"] : [],
-		define: {
-			GLOBALPATHS: JSON.stringify(templatePaths),
-		},
-		plugins: [
-			sassPlugin({
-				logger: {
-					warn: () => "",
-				},
-			}),
-			{
-				name: "external-files",
-				setup(inBuild) {
-					inBuild.onResolve({ filter: /(\.\/assets|\.\/fonts|\/systems)/ }, () => {
-						return { external: true };
-					});
-				},
+const production = process.env.NODE_ENV !== "development";
+const watch = process.argv.includes("--watch") && !production;
+
+const ctx = await context({
+	bundle: true,
+	entryPoints: ["./src/forbidden-lands.js", "./src/forbidden-lands.scss"],
+	outdir: "./",
+	format: "esm",
+	logLevel: "info",
+	sourcemap: !production ? "inline" : false,
+	ignoreAnnotations: !production,
+	minifyWhitespace: true,
+	minifySyntax: true,
+	drop: production ? ["console", "debugger"] : [],
+	define: {
+		GLOBALPATHS: JSON.stringify(templatePaths),
+	},
+	plugins: [
+		sassPlugin({
+			logger: {
+				warn: () => "",
 			},
-		],
-	});
+		}),
+		{
+			name: "external-files",
+			setup(inBuild) {
+				inBuild.onResolve({ filter: /(\.\/assets|\.\/fonts|\/systems)/ }, () => {
+					return { external: true };
+				});
+			},
+		},
+	],
+});
 
-	ctx.rebuild();
+ctx.rebuild();
 
-	if (watch) ctx.watch();
-	else ctx.dispose();
-};
+if (watch) ctx.watch();
+else ctx.dispose();
+
--- tools/get-template-paths.js
+++ tools/get-template-paths.js
@@ -2,7 +2,7 @@ import { glob } from "fs-extra-plus";
 import { posix, sep } from "node:path";
 
 export default (async () => {
-	const paths = await glob("**/*.hbs", { cwd: "." });
+	const paths = await glob("**/*.hbs", { cwd: "./templates" });
 	return paths.map((templatePath) => {
 		templatePath = templatePath.split(sep).slice(1).join(posix.sep);
 		return `systems/forbidden-lands/templates/${templatePath}`;
